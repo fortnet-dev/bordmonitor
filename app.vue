4f5518c85214dc503bda152cd75b4ee720ed7b64
Oskar Sommer <oskar.sommer@t-systems.com>
2025-01-24 15:53:28 +0100

    first ui drafts

--- app.vue
+++ app.vue
@@ -1,6 +1,87 @@
+<script setup lang="ts">
+import { UiButton } from "#components"
+import "~/assets/globals.scss"
+
+const enablePixelate = ref(true)
+const pixelateFactorPre = ref(0.35)
+const pixelateFactorPost = ref(0.25)
+</script>
+
 <template>
-	<div>
-		<NuxtRouteAnnouncer />
-		<NuxtWelcome />
-	</div>
+	<main>
+		<div
+			class="aspect-container"
+			:style="{ filter: enablePixelate ? 'url(#pixelate)' : '' }"
+		>
+			<UiButton>Bordcomputer</UiButton>
+		</div>
+
+		<div class="controls">
+			<label>
+				Pixelate
+				<input v-model="enablePixelate" type="checkbox" />
+			</label>
+			<label>
+				Pixelate factor pre ({{ pixelateFactorPre }})
+				<input v-model="pixelateFactorPre" type="range" min="0" max="1" step="0.01" />
+			</label>
+			<label>
+				Pixelate factor post ({{ pixelateFactorPost }})
+				<input v-model="pixelateFactorPost" type="range" min="0" max="1" step="0.01" />
+			</label>
+		</div>
+
+		<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="0" height="0">
+			<defs>
+				<filter id="pixelate" x="0" y="0">
+					<feFlood
+						x="2"
+						y="2"
+						:height="1 * pixelateFactorPre"
+						:width="1 * pixelateFactorPre"
+					/>
+					<feComposite :width="5 * pixelateFactorPre" :height="5 * pixelateFactorPre" />
+					<feTile result="a" />
+					<feComposite in="SourceGraphic" in2="a" operator="in" />
+					<feMorphology operator="dilate" :radius="2.5 * pixelateFactorPost" />
+				</filter>
+			</defs>
+		</svg>
+	</main>
 </template>
+
+<style scoped lang="scss">
+main {
+	background: hsl(0deg 0% 20%);
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+}
+
+.aspect-container {
+	background: var(--background);
+	aspect-ratio: 4 / 3;
+
+	--height: 600px;
+	height: var(--height);
+	font-size: calc(var(--height) / 20);
+
+	text-shadow: 1px 1px 2px black;
+	font-family: monospace, cursive;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	-webkit-font-smoothing: none;
+	// filter: url("#pixelate");
+	// transform: scale3d(3, 3, 1);
+}
+
+.controls {
+	display: flex;
+	flex-direction: column;
+}
+</style>
---
+++ components/ui/button.vue
@@ -0,0 +1,27 @@
+<template>
+	<button>
+		<slot />
+	</button>
+</template>
+
+<style scoped lang="scss">
+button {
+	background: hsl(0deg 0% 100% / 0.125);
+
+	padding: 0.125em 0.25em;
+
+	border-top: 0.2em solid white;
+	border-left: 0.2em solid white;
+	border-right: 0.2em solid black;
+	border-bottom: 0.2em solid black;
+
+	&:active {
+		background: hsl(0deg 0% 100% / 0);
+
+		border-top: 0.2em solid black;
+		border-left: 0.2em solid black;
+		border-right: 0.2em solid white;
+		border-bottom: 0.2em solid white;
+	}
+}
+</style>
