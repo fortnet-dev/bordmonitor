9a3ed8380082bc4d26fc3db2d264836561c4739f
Oskar Sommer <oskar.sommer@t-systems.com>
2025-01-25 17:27:41 +0100

    disable user select

--- app.vue
+++ app.vue
@@ -14,20 +14,21 @@ const pixelateFactorPost = ref(0.25)
 			:style="{ filter: enablePixelate ? 'url(#pixelate)' : '' }"
 		>
 			<UiButton>Bordcomputer</UiButton>
+			<UiButton>GPS-Navigation</UiButton>
 		</div>
 
 		<div class="controls">
 			<label>
-				Pixelate
 				<input v-model="enablePixelate" type="checkbox" />
+				Pixelate
 			</label>
 			<label>
-				Pixelate factor pre ({{ pixelateFactorPre }})
 				<input v-model="pixelateFactorPre" type="range" min="0" max="1" step="0.01" />
+				Pixelate factor pre ({{ pixelateFactorPre }})
 			</label>
 			<label>
-				Pixelate factor post ({{ pixelateFactorPost }})
 				<input v-model="pixelateFactorPost" type="range" min="0" max="1" step="0.01" />
+				Pixelate factor post ({{ pixelateFactorPost }})
 			</label>
 		</div>
 
@@ -60,6 +61,18 @@ main {
 	flex-direction: column;
 }
 
+@font-face {
+	font-family: "Roboto Mono Variable";
+	font-style: normal;
+	font-display: swap;
+	font-weight: 100 700;
+	src: url(https://cdn.jsdelivr.net/fontsource/fonts/roboto-mono:vf@latest/latin-wght-normal.woff2)
+		format("woff2-variations");
+	unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC,
+		U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215,
+		U+FEFF, U+FFFD;
+}
+
 .aspect-container {
 	background: var(--background);
 	aspect-ratio: 4 / 3;
@@ -69,11 +82,13 @@ main {
 	font-size: calc(var(--height) / 20);
 
 	text-shadow: 1px 1px 2px black;
-	font-family: monospace, cursive;
+	font-family: "Roboto Mono Variable", cursive;
 
 	display: flex;
 	justify-content: center;
 	align-items: center;
+	flex-direction: column;
+	gap: 1em;
 
 	-webkit-font-smoothing: none;
 	// filter: url("#pixelate");
